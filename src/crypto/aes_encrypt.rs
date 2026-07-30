51ae3821e586783862226ca200cc1bac31fb6687
Fajar Dimar Habibi <fajardimarh@gmail.com>
2026-07-30 12:35:45 +0700

    test(crypto/aes_encrypt): 6 unit tests untuk AES encrypt edge cases
    aes128_invalid_key_len, aes128_invalid_iv_len, aes128_empty_plaintext,
    aes256_invalid_key_len, aes256_cbc_encrypt_noiv
    
    Total: 913 -> 919 (all passing)

--- src/crypto/aes_encrypt.rs
+++ src/crypto/aes_encrypt.rs
@@ -117,4 +117,42 @@ mod tests {
             super::super::aes::aes256_cbc_decrypt(key, &encrypted[..16], &encrypted[16..]).unwrap();
         assert_eq!(&decrypted, plaintext);
     }
+
+    #[test]
+    fn aes128_invalid_key_len_returns_none() {
+        let key = b"short"; // not 16 bytes
+        let iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
+        assert!(aes_cbc_encrypt(key, iv, b"data").is_none());
+    }
+
+    #[test]
+    fn aes128_invalid_iv_len_returns_none() {
+        let key = b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10";
+        let iv = b"short"; // not 16 bytes
+        assert!(aes_cbc_encrypt(key, iv, b"data").is_none());
+    }
+
+    #[test]
+    fn aes128_empty_plaintext_returns_none() {
+        let key = b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10";
+        let iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
+        assert!(aes_cbc_encrypt(key, iv, b"").is_none());
+    }
+
+    #[test]
+    fn aes256_invalid_key_len_returns_none() {
+        let key = b"short"; // not 32 bytes
+        let iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
+        assert!(aes256_cbc_encrypt(key, iv, b"data").is_none());
+    }
+
+    #[test]
+    fn aes256_cbc_encrypt_noiv_ok() {
+        let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f";
+        let iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f";
+        let plaintext = b"Hello world!";
+        let result = aes256_cbc_encrypt_noiv(key, iv, plaintext);
+        assert!(!result.is_empty());
+        assert!(result.len() >= plaintext.len());
+    }
 }
